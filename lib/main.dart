9a5e85c511c3fb5131aa4c394f0de3e7d89ec2b7
masterUNG <phrombutr@gmail.com>
2018-11-14 10:36:01 +0700

    Column ok

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,39 @@ class MyApp extends StatelessWidget {
   var titleName = 'Basic Flutter';
   var messageString =
       'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of ';
-  var paddingDoule = 25.0;
+  var paddingDoule = 15.0;
 
   var friend1String = 'Doramon';
   var friend2String = 'Nopita';
   var friend3String = 'Sunako';
 
-  
+  var myFontSize = 25.0;
 
   @override
   Widget build(BuildContext context) {
-
     // Demo Create Text Widget
-    Text friend1Text = new Text(friend1String);
-    Text friend2Text = new Text(friend2String);
-    Text friend3Text = new Text(friend3String);
+    Text friend1Text = new Text(
+      friend1String,
+      style: new TextStyle(
+          color: Colors.pink[900],
+          fontSize: myFontSize,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic),
+    );
+    Text friend2Text = new Text(
+      friend2String,
+      style: new TextStyle(
+          color: Colors.deepOrange[900],
+          fontSize: myFontSize,
+          fontStyle: FontStyle.italic),
+    );
+    Text friend3Text = new Text(
+      friend3String,
+      style: new TextStyle(
+          color: Colors.blue,
+          fontSize: myFontSize,
+          fontStyle: FontStyle.normal),
+    );
 
     // Create Widget for body
     Widget widgetTextMessage = new Container(
@@ -31,14 +49,18 @@ class MyApp extends StatelessWidget {
       child: new Text(messageString),
     );
 
-    Widget widgetMyFiend = new Container(
+    Widget widgetMyFiend = new Container(padding: new EdgeInsets.all(paddingDoule),
       child: new Row(
         children: <Widget>[
-          friend1Text,
-          friend2Text,
-          friend3Text
+          new Expanded(
+            child: friend1Text,
+          ),
+          new Expanded(
+            child: friend2Text,
+          ),
+          new Expanded(child: friend3Text,)
         ],
-      ),
+      ), color: Colors.yellow,
     );
 
     return new MaterialApp(
@@ -47,7 +69,7 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(titleName),
         ),
-        body: widgetMyFiend,
+        body: new Container(child: new Column(children: <Widget>[widgetMyFiend, widgetMyFiend, widgetTextMessage],),),
       ),
     );
   }
